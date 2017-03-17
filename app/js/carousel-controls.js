1a2ce0122dc27f909373562277406367e2dbdc02
Oscar Rodriguez <oscarrobertrodriguez@gmail.com>
2017-03-16 22:21:16 -0500

    a lot of changes

--- app/js/carousel-controls.js
+++ app/js/carousel-controls.js
@@ -2,5 +2,23 @@
 
 var backBtn = document.querySelector(".back-button");
 var forwardBtn = document.querySelector(".forward-button");
+var carousel = document.querySelector(".js-carousel");
+
 console.log(backBtn);
 console.log(forwardBtn);
+
+
+
+forwardBtn.addEventListener("click", forward, false);
+
+backBtn.addEventListener("click", back, false);
+
+
+function forward () {
+    carousel.className += "js-carousel--turn";
+}
+
+
+function back () {
+	console.log("back");
+}
